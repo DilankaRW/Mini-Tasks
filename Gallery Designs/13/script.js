1fe014b75004a591ea4e0e1bcede01e1e2a6f665
VL.dilankarw <dilankarashmika10@gmail.com>
2024-05-20 00:13:10 +0530

    Design 13

---
+++ Gallery Designs/13/script.js
@@ -0,0 +1,10 @@
+const $container = document.querySelector('.container');
+
+setInterval(
+    () => {
+        const $last = $container.firstElementChild;
+        $last.remove();
+        $container.appendChild($last);
+    },
+    1000,
+);
