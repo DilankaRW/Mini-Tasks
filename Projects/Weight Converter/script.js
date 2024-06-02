fc37c7c10e2d625fbe5e65b0f941cf7710db76ea
VL.dilankarw <dilankarashmika10@gmail.com>
2024-06-03 00:06:37 +0530

    Weight Converter commit

--- Projects/Weight Converter/script.js
+++ Projects/Weight Converter/script.js
@@ -5,6 +5,23 @@ const oz = document.querySelector('.oz');
 
 const convert ={
     lb: (val) =>{
-        kg.valuw = val * 0.45359237;
+        kg.value = val * 0.45359237;
+        oz.value = val * 16;
+    },
+    kg: (val) =>{
+        lb.value = val * 2.2;
+        oz.value = val * 35.274;
+    },
+    oz: (val) =>{
+        lb.value = val * 0.0625;
+        kg.value = val * 0.02834952;
     }
 }
+
+window.addEventListener(
+    'input',
+    (e) => {
+        convert[e.target.classList[0]]
+        (e.target.value);
+    }
+);
